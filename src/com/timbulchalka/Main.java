7665da67d26ab0048d6055ac4136ea47674dac19
dccfmatos <d.catarina.c.f@gmail.com>
2019-04-26 16:15:35 +0100

    3rd commit - void

--- src/com/timbulchalka/Main.java
+++ src/com/timbulchalka/Main.java
@@ -3,24 +3,23 @@ package com.timbulchalka;
 public class Main {
 
     public static void main(String[] args) {
-	boolean gameOver = true;
-	int score = 800;
-	int levelCompleted = 5;
-	int bonus = 100;
+		calculateScore(true, 800, 5, 100); // go run the method defined bellow
+		calculateScore(true, 10000, 8, 200);
+	}
+    //method main above
+	//new method bellow, must be inside class Main
 
-	if(gameOver){
-	    int finalScore = score + (levelCompleted * bonus);
-	    finalScore += 1000;
-        System.out.println("Your final score was" + finalScore);
-    }
+	public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) { //method parameters
 
-	score = 10000;
-	levelCompleted = 8;
-	bonus = 200;
+    	//use void not to return any information from the method
+		if(gameOver){
+			int finalScore = score + (levelCompleted * bonus);
+			finalScore += 2000;
+			System.out.println("Your final score was " + finalScore);
+		}
 
-	if(gameOver){
-	    int finalScore = score + (levelCompleted * bonus);
-        System.out.println("Your final score was" + finalScore);
-    }
-    }
+	}
+
+
+
 }
