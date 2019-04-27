206787d3500d3a65ab216de7a10af2e67c51e905
dccfmatos <d.catarina.c.f@gmail.com>
2019-04-27 18:51:35 +0100

    5th - class 31

--- src/com/timbulchalka/Main.java
+++ src/com/timbulchalka/Main.java
@@ -3,8 +3,24 @@ package com.timbulchalka;
 public class Main {
 
     public static void main(String[] args) {
-		calculateScore(true, 800, 5, 100); // go run the method defined bellow
-		calculateScore(true, 10000, 8, 200);
+		int highScore = calculateScore(true, 800, 5, 100);
+		// go run the method defined bellow Variable highScore will have the value returned
+		System.out.println("Your final score was " + highScore);
+
+		highScore = calculateScore(true, 10000, 8, 200);
+		System.out.println("Your final score was " + highScore);
+
+		int position = calculateHighScorePosition(1500);
+		displayHighScorePosition("Diana", position);
+
+		position = calculateHighScorePosition(900);
+		displayHighScorePosition("João", position);
+
+		position = calculateHighScorePosition(400);
+		displayHighScorePosition("Max", position);
+
+		position = calculateHighScorePosition(50);
+		displayHighScorePosition("Brutus", position);
 	}
     //method main above
 	//new method bellow, must be inside class Main
@@ -14,11 +30,9 @@ public class Main {
     	//use void not to return any information from the method
 		//use type of data to return from method (example: int)
 
-
 		if(gameOver){
 			int finalScore = score + (levelCompleted * bonus);
 			finalScore += 2000;
-			System.out.println("Your final score was " + finalScore);
 			return finalScore;
 		}//my solution::
 		// else{
@@ -28,8 +42,22 @@ public class Main {
 		return -1;
 		}
 
+		//EXERCISE
+
+	public static void displayHighScorePosition (String playerName, int position ){
+		System.out.println(playerName + " managed to get into position " + position + " on the Highscore table.");
 	}
 
+	public static int calculateHighScorePosition(int playerScore) {
 
-
-}
+    	if (playerScore >= 1000){
+    		return 1;
+		}else if (playerScore >= 500 && playerScore < 1000){
+			return 2;
+				} else if (playerScore >= 100 && playerScore <500){
+    		return 3;
+		} else {
+    		return 4;
+		}
+	}
+	}
